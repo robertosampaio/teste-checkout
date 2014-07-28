21c2c24cb201f7f76112c05e4a4956a167863f9d
rsampaio <rsampaio@appi.com.br>
2014-07-28 10:30:28 -0300

    Subindo projeto de teste

---
+++ testeCheckout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testeCheckout
+//
+//  Created by rsampaio on 28/07/14.
+//  Copyright (c) 2014 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testeCheckout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testeCheckout
+//
+//  Created by rsampaio on 28/07/14.
+//  Copyright (c) 2014 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
